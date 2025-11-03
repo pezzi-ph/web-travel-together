5aebde5f6328f0ddbbf2c18ecb952fdcee2dac07
pezzi-ph <pezzi.hartlieb@gmail.com>
2025-11-03 18:43:05 +0100

    model improvement

--- travel-together/model.py
+++ travel-together/model.py
@@ -1,15 +1,18 @@
 import datetime
-from typing import List, Optional
+from typing import List
 
 from sqlalchemy import String, DateTime, ForeignKey, Integer, Boolean
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.sql import func
 
+import flask_login
+
+
 from . import db
 
 import enum
 
-class User(db.Model):
+class User(flask_login.UserMixin, db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(128), unique=True)
     name: Mapped[str] = mapped_column(String(64))
@@ -20,7 +23,8 @@ class User(db.Model):
 class TripProposal(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     departures: Mapped[List["Location"]] = relationship(back_populates="Location")
-    destination:  Mapped[int] = mapped_column(ForeignKey("Location.id"))
+    destinationId:  Mapped[int] = mapped_column(ForeignKey("Location.id"))
+    destination: Mapped["Location"] = relationship(back_populates="Location")
     possibleDates: Mapped[List["DateProposal"]] = relationship(back_populates="Location")
     budget: Mapped[int] = mapped_column(Integer)
     activities: Mapped[List["Activity"]] = relationship(back_populates="Activity")
@@ -35,22 +39,28 @@ class TripProposal(db.Model):
 class Message(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     text: Mapped[str] = mapped_column(String(256))
-    timestamp: Mapped[datetime.datetime] = mapped_column(DateTime)
-    author: Mapped[int] = mapped_column(ForeignKey("User.id"))
-    trip: Mapped[int] = mapped_column(ForeignKey("TripProposal.id"))
+    timestamp: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    authorId: Mapped[int] = mapped_column(ForeignKey("TripProposalParticipant.id"))
+    author: Mapped["TripProposalParticipant"] = relationship(back_populates="TripProposalParticipant")
+
 
 class TripProposalParticipant(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    user: Mapped[int] = mapped_column(ForeignKey("User.id"))
-    trip: Mapped[int] = mapped_column(ForeignKey("TripProposal.id"))
+    userId: Mapped[int] = mapped_column(ForeignKey("User.id"))
+    user: Mapped["User"] = relationship(back_populates="User")
+    tripId: Mapped[int] = mapped_column(ForeignKey("TripProposal.id"))
+    trip: Mapped["TripProposal"] = relationship(back_populates="TripProposal")
     canEdit: Mapped[bool] = mapped_column(Boolean)
 
 class Meetup(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     meetupTime: Mapped[datetime.datetime] = mapped_column(DateTime)
-    location: Mapped[int] = mapped_column(ForeignKey("Location.id"))
-    trip: Mapped[int] = mapped_column(ForeignKey("TripProposal.id"))
-    createdBy: Mapped[int] = mapped_column(ForeignKey("User.id"))
+    locationId: Mapped[int] = mapped_column(ForeignKey("Location.id"))
+    location: Mapped["Location"] = relationship(back_populates="Location")
+    tripId: Mapped[int] = mapped_column(ForeignKey("TripProposal.id"))
+    trip: Mapped["TripProposal"] = relationship(back_populates="TripProposal")
+    createdById: Mapped[int] = mapped_column(ForeignKey("User.id"))
+    createdBy: Mapped["User"] = relationship(back_populates="User")
 
 class Activity(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
