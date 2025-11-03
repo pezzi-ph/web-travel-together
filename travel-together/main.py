bc5971e2fb90eba9100820e6d97e18ae21a2f45e
pezzi-ph <pezzi.hartlieb@gmail.com>
2025-11-03 17:25:17 +0100

    basic setup

---
+++ travel-together/main.py
@@ -0,0 +1,14 @@
+import datetime
+import dateutil.tz
+
+from flask import Blueprint, render_template
+
+
+from . import model
+
+bp = Blueprint("main", __name__)
+
+
+@bp.route("/")
+def index():
+    return render_template("main/index.html")
---
+++ travel-together/model.py
@@ -0,0 +1,5 @@
+class User:
+    def __init__(self, user_id, email, name):
+        self.user_id = user_id
+        self.email = email
+        self.name = name
